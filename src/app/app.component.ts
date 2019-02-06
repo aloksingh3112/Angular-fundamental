f367cbe019e40a019cdc88d3954f08156569b90a
aloksingh3112 <aloksingh3112@gmail.com>
2019-02-06 15:11:10 +0530

    added ngIf working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-fundamentals';
+
+  data:string ='';
+  changeInput(value:string){
+    this.data=value
+  }
 }
